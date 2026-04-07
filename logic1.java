9507dd6358953a8596a98d39ee80c4b3002c14cc
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-07 10:50:19 +0400

    add task 2 caughtSpeeding

--- logic1.java
+++ logic1.java
@@ -1,7 +1,13 @@
 public class logic1 {
     public static void main(String[] args){
-        logic1 ex1= new logic1();
-       System.out.println(ex1.cigarParty(70,true));
+        // task 1
+      /*  logic1 ex1= new logic1();
+       System.out.println(ex1.cigarParty(70,true));*/
+
+        // task 2
+        logic1 ex2= new logic1();
+        System.out.println(ex2.caughtSpeeding(86,true));
+
 
     }
 public boolean cigarParty(int cigars, boolean isWeekend){
@@ -20,5 +26,24 @@ public boolean cigarParty(int cigars, boolean isWeekend){
         }
 
 }
+
+    public int caughtSpeeding(int speed, boolean isBirthday){
+        if(isBirthday ){
+            if(speed <= 65){
+                return 0;
+            } else if (speed>=66 && speed<= 85) {
+                return 1;
+            }else {
+                return 2;
+            }
+        } else if (speed<= 60) {
+            return 0;
+        }else if(speed>= 60 && speed <=80){
+            return 1;
+        }else{
+            return 2;
+        }
+
+    }
 
 }
