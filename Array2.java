049556eeeb3aec1a71a1a950584532aa4ce40c9c
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-08 09:59:31 +0400

    add task 2 sum13

--- Array2.java
+++ Array2.java
@@ -4,8 +4,12 @@ public class Array2 {
 
         Array2 ex = new Array2();
         // task 1
-        int [] arr = {2, 1, 2, 3, 4};
-       System.out.println(ex.countEvens(arr));
+        /*int [] arr = {2, 1, 2, 3, 4};
+       System.out.println(ex.countEvens(arr));*/
+
+        // task 2
+        int [] arr = {2, 1, 13, 3, 4};
+        System.out.println(ex.sum13(arr));
 
     }
     public int  countEvens(int [] arr){
@@ -17,6 +21,18 @@ public class Array2 {
             }
         }
         return count;
+    }
+
+    public int sum13(int [] arr ){
+        int total = 0;
+        for(int i =0; i<arr.length;i++){
+            if(arr[i]== 13){
+                i++;
+            }else {
+                total+= arr[i];
+            }
+        }
+        return total;
     }
 
 }
